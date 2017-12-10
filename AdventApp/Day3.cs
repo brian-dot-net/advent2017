4b19f1ca22df20c1e75930cbf8d341f9a2e37061
Brian Rogers <bobbymcr@gmail.com>
2017-12-10 11:32:30 -0800

    Prepare for Day 3 part B

--- AdventApp/Day3.cs
+++ AdventApp/Day3.cs
@@ -1,5 +1,7 @@
 ﻿namespace Advent
 {
+    using System;
+
     public abstract class Day3 : ICanRun
     {
         public string DefaultInput
@@ -13,5 +15,78 @@
         }
 
         protected abstract int RunCore(string input);
+
+        protected sealed class Steps
+        {
+            private int remaining;
+            private int stride;
+            private Direction dir;
+            private int x;
+            private int y;
+            private int step;
+
+            public Steps(int n)
+            {
+                this.remaining = n - 1;
+            }
+
+            public int Distance
+            {
+                get => Math.Abs(this.x) + Math.Abs(this.y);
+            }
+
+            public bool MoveMany()
+            {
+                this.Turn();
+                bool hasMore = this.remaining > this.stride;
+                if (!hasMore)
+                {
+                    this.stride = this.remaining;
+                }
+
+                this.MoveNow();
+                this.remaining -= this.stride;
+
+                return hasMore;
+            }
+
+            private void Turn()
+            {
+                this.dir = (Direction)(((int)this.dir + 1) % 4);
+                if (this.step % 2 == 0)
+                {
+                    ++this.stride;
+                }
+
+                ++this.step;
+            }
+
+            private void MoveNow()
+            {
+                switch (this.dir)
+                {
+                    case Direction.Down:
+                        this.y -= this.stride;
+                        break;
+                    case Direction.Right:
+                        this.x += this.stride;
+                        break;
+                    case Direction.Up:
+                        this.y += this.stride;
+                        break;
+                    case Direction.Left:
+                        this.x -= this.stride;
+                        break;
+                }
+            }
+
+            private enum Direction
+            {
+                Down,
+                Right,
+                Up,
+                Left
+            }
+        }
     }
 }
--- AdventApp/Day3A.cs
+++ AdventApp/Day3A.cs
@@ -1,91 +1,16 @@
 ﻿namespace Advent
 {
-    using System;
-
     public sealed class Day3A : Day3
     {
         protected override int RunCore(string input)
         {
             int n = int.Parse(input);
             Steps steps = new Steps(n);
-            while (steps.MoveNext())
+            while (steps.MoveMany())
             {
             }
 
             return steps.Distance;
-        }
-
-        private sealed class Steps
-        {
-            private int remaining;
-            private int stride;
-            private Direction dir;
-            private int x;
-            private int y;
-            private int step;
-
-            public Steps(int n)
-            {
-                this.remaining = n - 1;
-            }
-
-            public int Distance
-            {
-                get => Math.Abs(this.x) + Math.Abs(this.y);
-            }
-
-            public bool MoveNext()
-            {
-                this.Turn();
-                bool hasMore = this.remaining > this.stride;
-                if (!hasMore)
-                {
-                    this.stride = this.remaining;
-                }
-
-                this.MoveNow();
-                this.remaining -= this.stride;
-
-                return hasMore;
-            }
-
-            private void Turn()
-            {
-                this.dir = (Direction)(((int)this.dir + 1) % 4);
-                if (this.step % 2 == 0)
-                {
-                    ++this.stride;
-                }
-
-                ++this.step;
-            }
-
-            private void MoveNow()
-            {
-                switch (this.dir)
-                {
-                    case Direction.Down:
-                        this.y -= this.stride;
-                        break;
-                    case Direction.Right:
-                        this.x += this.stride;
-                        break;
-                    case Direction.Up:
-                        this.y += this.stride;
-                        break;
-                    case Direction.Left:
-                        this.x -= this.stride;
-                        break;
-                }
-            }
-
-            private enum Direction
-            {
-                Down,
-                Right,
-                Up,
-                Left
-            }
         }
     }
 }
